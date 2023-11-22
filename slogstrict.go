45700ae684305a209f2eae640a7824b059fe6d6a
Travis Bale <travis.l.bale@gmail.com>
2023-11-22 12:58:55 -0500

    fix: add source displays wrapping function

--- slogstrict.go
+++ slogstrict.go
@@ -3,6 +3,8 @@ package slogstrict
 import (
 	"context"
 	"log/slog"
+	"runtime"
+	"time"
 )
 
 // A limited set of method from slog.Logger
@@ -44,25 +46,35 @@ func (s logger) WithGroup(name string) Logger {
 	return logger{s.Logger.WithGroup(name)}
 }
 
-func (s logger) Log(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
-	s.Logger.LogAttrs(ctx, level, msg, attrs...)
-}
-
 func (s logger) Debug(ctx context.Context, msg string, attrs ...slog.Attr) {
-	s.Logger.LogAttrs(ctx, slog.LevelDebug, msg, attrs...)
+	s.log(ctx, slog.LevelDebug, msg, attrs...)
 }
 
 func (s logger) Info(ctx context.Context, msg string, attrs ...slog.Attr) {
-	s.Logger.LogAttrs(ctx, slog.LevelInfo, msg, attrs...)
+	s.log(ctx, slog.LevelInfo, msg, attrs...)
 }
 
 func (s logger) Warn(ctx context.Context, msg string, attrs ...slog.Attr) {
-	s.Logger.LogAttrs(ctx, slog.LevelWarn, msg, attrs...)
+	s.log(ctx, slog.LevelWarn, msg, attrs...)
 }
 
 func (s logger) Error(ctx context.Context, msg string, err error, attrs ...slog.Attr) {
 	if err != nil {
 		attrs = append([]slog.Attr{slog.String("err", err.Error())}, attrs...)
 	}
-	s.Logger.LogAttrs(ctx, slog.LevelError, msg, attrs...)
+	s.log(ctx, slog.LevelError, msg, attrs...)
+}
+
+func (s logger) Log(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
+	s.log(ctx, level, msg, attrs...)
+}
+
+func (s logger) log(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
+	if !s.Logger.Enabled(ctx, level) {
+		return
+	}
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:]) // 3 is used as this how deep we are in the call stack [Caller, (Level), log]
+	r := slog.NewRecord(time.Now(), slog.LevelInfo, msg, pcs[0])
+	_ = s.Logger.Handler().Handle(ctx, r)
 }
