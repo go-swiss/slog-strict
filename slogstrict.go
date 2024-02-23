879ae786a2e585bf6832a57519a86ddbd663e86b
Stephen Afam-Osemene <me@stephenafamo.com>
2024-02-23 17:12:37 +0000

    Add correct source information

--- slogstrict.go
+++ slogstrict.go
@@ -3,6 +3,8 @@ package slogstrict
 import (
 	"context"
 	"log/slog"
+	"runtime"
+	"time"
 )
 
 // A limited set of method from slog.Logger
@@ -48,26 +50,42 @@ func (s logger) WithGroup(name string) Logger {
 }
 
 func (s logger) Log(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
-	s.Logger.LogAttrs(ctx, level, msg, attrs...)
+	s.logAttrs(ctx, level, msg, attrs...)
 }
 
 func (s logger) Debug(ctx context.Context, msg string, attrs ...slog.Attr) {
-	s.Logger.LogAttrs(ctx, slog.LevelDebug, msg, attrs...)
+	s.logAttrs(ctx, slog.LevelDebug, msg, attrs...)
 }
 
 func (s logger) Info(ctx context.Context, msg string, attrs ...slog.Attr) {
-	s.Logger.LogAttrs(ctx, slog.LevelInfo, msg, attrs...)
+	s.logAttrs(ctx, slog.LevelInfo, msg, attrs...)
 }
 
 func (s logger) Warn(ctx context.Context, msg string, attrs ...slog.Attr) {
-	s.Logger.LogAttrs(ctx, slog.LevelWarn, msg, attrs...)
+	s.logAttrs(ctx, slog.LevelWarn, msg, attrs...)
 }
 
 func (s logger) Error(ctx context.Context, msg string, err error, attrs ...slog.Attr) {
 	if err != nil {
 		attrs = append([]slog.Attr{slog.String("err", err.Error())}, attrs...)
 	}
-	s.Logger.LogAttrs(ctx, slog.LevelError, msg, attrs...)
+	s.logAttrs(ctx, slog.LevelError, msg, attrs...)
+}
+
+func (l logger) logAttrs(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
+	if !l.Enabled(ctx, level) {
+		return
+	}
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:])
+
+	r := slog.NewRecord(time.Now(), level, msg, pcs[0])
+	r.AddAttrs(attrs...)
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	_ = l.Handler().Handle(ctx, r)
 }
 
 func (s logger) ToSlog() *slog.Logger {
